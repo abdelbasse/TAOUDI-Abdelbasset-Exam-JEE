a0ccbb1fd117ea8377a035837eb0871416a9f3c4
abdelbasse <ABD.TAOUDI-ETU@etu.univh2c.ma>
2025-05-19 08:59:09 +0100

    first commit

---
+++ exam-final-jee-backend/src/main/java/com/example/exam_final_jee/ExamFinalJeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.exam_final_jee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamFinalJeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamFinalJeeApplication.class, args);
+	}
+
+}
---
+++ exam-final-jee-backend/src/test/java/com/example/exam_final_jee/ExamFinalJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exam_final_jee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamFinalJeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
